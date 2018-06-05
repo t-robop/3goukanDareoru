d3c57fabe6397703693a29b3b9897ada4427637d
fujitaken <kentyan199@yahoo.co.jp>
2018-06-05 19:09:11 +0900

    add int by test-fuji

--- app/src/main/java/com/example/fujitaken/a3goukandareoruapp/MainActivity.java
+++ app/src/main/java/com/example/fujitaken/a3goukandareoruapp/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int aa = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
