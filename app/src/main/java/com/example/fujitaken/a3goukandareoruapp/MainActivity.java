17fca34705ac8c0cecc9c4a1f07a618aba874c0c
fujitaken <kentyan199@yahoo.co.jp>
2018-06-05 18:31:31 +0900

    testaaa

---
+++ app/src/main/java/com/example/fujitaken/a3goukandareoruapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fujitaken.a3goukandareoruapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
