0a76c1984c703a92354f98902899389a7d880baf
fujitaken <kentyan199@yahoo.co.jp>
2018-06-12 18:35:44 +0900

    javaファイルの中身を編集した

--- app/src/main/java/com/example/fujitaken/a3goukandareoruapp/InActivity.java
+++ app/src/main/java/com/example/fujitaken/a3goukandareoruapp/InActivity.java
@@ -1,4 +1,14 @@
 package com.example.fujitaken.a3goukandareoruapp;
 
-public class InActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.in_activity);
+
+    }
 }
--- app/src/main/java/com/example/fujitaken/a3goukandareoruapp/LogActivity.java
+++ app/src/main/java/com/example/fujitaken/a3goukandareoruapp/LogActivity.java
@@ -1,4 +1,14 @@
 package com.example.fujitaken.a3goukandareoruapp;
 
-public class LogActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_activity);
+
+    }
 }
